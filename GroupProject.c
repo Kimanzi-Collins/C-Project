353aa9dc08a3aa43374ce4b00a9585367fba1792
Coll0 <mwandikwacollins@gmail.com>
2026-01-08 11:49:08 +0300

    Added Daily Transport Tracker Logic File. Subject to change

---
+++ GroupProject.c
@@ -0,0 +1,39 @@
+//SCS3/2490/2025
+//SCS3/9999/2025
+//SCS3/2589/2025
+//SCS3/150356/2025
+#include <stdio.h>
+
+int main(){
+    int choice;
+    do{
+        printf("-------------Daily Transport Tracker-------------\n");
+        printf("Welcome to the Daily Transport Tracker System!\n");
+        printf("This system helps you track daily transport expenses efficiently.\n");
+        printf("You can log your daily transport costs and generate reports as needed.\n");
+        printf("Please select an option:\n");
+        printf("1. Log Daily Transport Expense\n");
+        printf("2. Generate Expense Report\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1:
+                // Function to log daily transport expense
+                printf("Logging daily transport expense...\n");
+                logExpense();
+                break;
+            case 2:
+                // Function to generate expense report
+                printf("Generating expense report...\n");
+                generateReport();
+                break;
+            case 3:
+                printf("Exiting the system. Goodbye!\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while(choice != 3);
+}
+    
